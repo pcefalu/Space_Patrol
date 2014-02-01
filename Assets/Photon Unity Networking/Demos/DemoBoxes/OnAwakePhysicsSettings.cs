7df2938c0be53e210a0e15c9ec024b0af43ed16b
Paul Cefalu <pcefalu@fullsail.edu>
2014-02-01 02:27:43 -0500

    Creation...

---
+++ Assets/Photon Unity Networking/Demos/DemoBoxes/OnAwakePhysicsSettings.cs
@@ -0,0 +1,21 @@
+using System.Threading;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PhotonView))]
+public class OnAwakePhysicsSettings : Photon.MonoBehaviour {
+
+    // this tiny script "disables" the rigidbody for remotely controlled GameObjects (owned by someone else)
+    void Awake()
+    {
+        if (!this.photonView.isMine)
+        {
+            Rigidbody attachedRigidbody = this.GetComponent<Rigidbody>();
+            if (attachedRigidbody != null)
+            {
+                attachedRigidbody.isKinematic = true;
+            }
+        }
+    }
+}
